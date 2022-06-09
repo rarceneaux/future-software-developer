1689e276289694f1402ec8f56f17a1f0487d32cc
Raymond <40898992+rarceneaux@users.noreply.github.com>
2022-06-09 10:40:14 -0500

    added files

---
+++ main.js
@@ -0,0 +1,103 @@
+const jumboDiv = document.getElementById('jumbo');
+const formDiv = document.getElementById('form');
+const cardDiv = document.getElementById('card');
+const makeMagicBtn = document.getElementById('makeMagic');
+const sortBtn = document.getElementById('sort');
+const inputBox = document.getElementById('nameInputBox');
+
+let amazonLogo = 'amazon-logo.png';
+
+// Print to DOM
+const PTD = (stringToPrint,divId) => {
+  const selectedDiv = document.getElementById(divId);
+  selectedDiv.innerHTML += stringToPrint;
+}
+// House Array
+const houses = ['AMAZON','GOOGLE','TIK TOK','YOUTUBE','APPLE'];
+
+// Make Jumbo runs on page load
+const makeJumbo = (e) => {
+  const JumboString =`<div class="jumbotron">
+  <h1 class="display-4">Future Software Developers 💻</h1>
+  <h2 >   </h2>
+  <hr class="my-4">
+  <H1>What Tech company will hire you....</H1>
+  <a  id='makeMagic'class="btn btn-success btn-lg" role="button">Let's Find Out</a>
+</div>`;
+PTD(JumboString,'jumbo');
+}
+makeJumbo();
+
+// Being called on line 71
+const makeForm = (e) => {
+  document.getElementById('form').innerHTML = '';
+  let formString =`
+<form class="form-inline">
+    <div class="">
+      <input type="text" readonly class="form-control-plaintext" id="name" value="Enter Your Name">
+    </div>
+    <div id="inputDiv">
+      <input type="text" id="nameInputBox" placeholder="Name" required="">
+    </div>
+    <button  id="sort" class="btn btn-success mb-2">PLAY</button>
+  </form>`;
+  PTD(formString,'form')
+document.getElementById('nameInputBox') .focus();
+studentBtnEvent();
+$('#form').show();
+};
+
+
+//  Being Called on Line 75
+const makeCard = (e) => {
+  const studentName = nameInputBox.value;
+    let randomHouse = houses[Math.floor(Math.random() *houses.length)];
+    let cardPrint = `<div class="card" style="width: 20rem;">
+    <h1 class="card-header text-center">Welcome to ${randomHouse}</h1>
+    <div class="card-body text-center">
+    <form>
+      <h1 class="studentName text-center">${studentName}</h1>
+          <div class="form-group">
+            <label for="formGroupExampleInput"></label>
+            <h2 type="text"  id="house">🎉🎉🎉</h2>
+
+          </div>
+        </form>
+      <button  type="submit" class="expel">Delete</button>
+      </div>
+  </div>`;
+  PTD(cardPrint,'card');
+  removeCard();
+$('#sort').attr('disabled',true);
+nameInputBox.value = '';
+};
+
+
+// Calling after Jumbo and make magic is created in Make Jumbo FN
+const makeMagicBtnEvent = (e) => {
+document.getElementById('makeMagic').addEventListener('click',makeForm);
+} 
+makeMagicBtnEvent();
+
+// Called @ Line 39 Make Form FN
+const studentBtnEvent = () => {
+document.getElementById('sort').addEventListener('click', function (e){
+e.preventDefault();
+$('#form').hide();
+makeCard();
+  })
+};
+
+// Calling on line 62 in Make Card FN
+const removeCard = () =>{
+  const cardsToRemove = document.getElementsByClassName('expel');
+  for (let i =0;i<cardsToRemove.length;i++){
+    const deleteMe = cardsToRemove[i];
+    deleteMe.addEventListener('click' ,(e) =>{
+      const btnClicked = e.target;
+      const cardToRemove = btnClicked.parentNode.parentNode;
+      cardToRemove.remove();
+    })
+  }
+};
+
